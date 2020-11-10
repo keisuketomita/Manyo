749ff169f6bb35dac1e7859ee66fc997f31b23ba
keisuketomita <keisuke.tommy@gmail.com>
2020-11-10 17:44:46 +0900

    task index,show

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,46 @@
+class TasksController < ApplicationController
+  before_action :set_task, only: [:edit, :update, :destroy, :show]
+  def index
+    @tasks = Task.all
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    if @task.save
+      redirect_to tasks_path, notice:"タスクを登録しました"
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @task.update(task_params)
+      redirect_to tasks_path, notice: "タスクを編集しました"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @task.destroy
+    redirect_to tasks_path, notice: "タスクを削除しました"
+  end
+
+  def show
+  end
+
+  private
+  def task_params
+    params.require(:task).permit(:name, :detail)
+  end
+  def set_task
+     @task = Task.find(params[:id])
+  end
+end
