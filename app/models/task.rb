ce67d7b39b5bbba39ffbf82540d3a8ce8b8857d0
keisuketomita <keisuke.tommy@gmail.com>
2020-11-17 11:34:32 +0900

    add:ErrorPage, edit:search_task_label, new_task_label

--- app/models/task.rb
+++ app/models/task.rb
@@ -8,12 +8,15 @@ class Task < ApplicationRecord
   scope :priority_desc, -> { order(priority: :desc) }
   scope :dead_line_desc, -> { order(dead_line: :desc) }
   scope :created_at_desc, -> { order(created_at: :desc) }
-  scope :name_like, -> (name) { where('name LIKE ?', "%#{name}%") if name.present? }
+  scope :name_like, -> (name) { where('tasks.name LIKE ?', "%#{name}%") if name.present? }
   scope :status_is, -> (status) { where(status: status) if status.present? }
+  scope :label_is, -> (labels) { joins(:labels).where(labels: { id: labels }) if labels.present? }
 
   enum priority: { 低: 0, 中: 1, 高: 2 }
 
   belongs_to :user
   validates_presence_of :user
+  has_many :labellings, dependent: :destroy
+  has_many :labels, through: :labellings
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,35 +5,51 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# *** タスク51個作成
 # 51.times do |n|
 #   name = Faker::Games::Pokemon.name
 #   detail = Faker::Games::Pokemon.name
 #   dead_line = "002020-11-30"
 #   status = "未着手"
 #   priority = 0
+#   user_id = 1
 #   Task.create!(name: name,
 #                detail: detail,
 #                dead_line: dead_line,
 #                status: status,
-#                priority: priority
+#                priority: priority,
+#                user_id: user_id
 #                )
 # end
-name = "hogea"
-email = "hogea@hoge.jp"
-password = "hogehoge"
-User.create!(name: name,
-             email: email,
-             password: password,
-             password_confirmation: password
-             )
 
-name = "hogeb"
-email = "hogeb@hoge.jp"
-password = "hogehoge"
-admin = "true"
-User.create!(name: name,
-             email: email,
-             password: password,
-             password_confirmation: password,
-             admin: admin
-             )
+# *** 一般ユーザー1人作成
+# name = "hogea"
+# email = "hogea@hoge.jp"
+# password = "hogehoge"
+# User.create!(name: name,
+#              email: email,
+#              password: password,
+#              password_confirmation: password
+#              )
+#
+# # *** 管理ユーザー1人作成
+# name = "hogeb"
+# email = "hogeb@hoge.jp"
+# password = "hogehoge"
+# admin = "true"
+# User.create!(name: name,
+#              email: email,
+#              password: password,
+#              password_confirmation: password,
+#              admin: admin
+#              )
+
+# *** ラベル作成
+@count = 1
+3.times do |n|
+  name = " ラベル#{@count} "
+  Label.create!(name: name,
+               )
+  @count += 1
+end
