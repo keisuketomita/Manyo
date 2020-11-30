47d54f35b173a299e35c49ba5c1f6623dcfe0826
keisuketomita <keisuke.tommy@gmail.com>
2020-11-30 14:54:10 +0900

    add seeds-deta for search-index

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,18 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+# *** 管理ユーザー1人作成
+name = "admin"
+email = "admin@hoge.jp"
+password = "hogehoge"
+admin = "true"
+User.create!(
+  name: name,
+  email: email,
+  password: password,
+  password_confirmation: password,
+  admin: admin
+)
 # *** 一般ユーザー10人,タスク10件作成
 @count = 2
 10.times do |n|
@@ -38,18 +50,6 @@
   @count += 1
 end
 
-# *** 管理ユーザー1人作成
-name = "admin"
-email = "admin@hoge.jp"
-password = "hogehoge"
-admin = "true"
-User.create!(
-  name: name,
-  email: email,
-  password: password,
-  password_confirmation: password,
-  admin: admin
-)
 
 # *** ラベル作成
 @count = 1
@@ -57,4 +57,52 @@ User.create!(
   name = " ラベル#{@count} "
   Label.create!(name: name)
   @count += 1
+end
+
+
+# *** 検索インデックス検証用
+name = "test"
+email = "test@hoge.jp"
+password = "hogehoge"
+@user = User.create!(
+  name: name,
+  email: email,
+  password: password,
+  password_confirmation: password
+)
+
+1000.times do |n|
+  10.times do |n|
+    name = Faker::Games::Pokemon.name
+    detail = Faker::Games::Pokemon.name
+    dead_line = "002020-11-30"
+    status = "未着手"
+    priority = 0
+    user_id = @user.id
+    Task.create!(
+      name: name,
+      detail: detail,
+      dead_line: dead_line,
+      status: status,
+      priority: priority,
+      user_id: user_id
+    )
+  end
+
+  10.times do |n|
+    name = Faker::Color.name
+    detail = Faker::Color.name
+    dead_line = "002020-12-30"
+    status = "着手中"
+    priority = 1
+    user_id = @user.id
+    Task.create!(
+      name: name,
+      detail: detail,
+      dead_line: dead_line,
+      status: status,
+      priority: priority,
+      user_id: user_id
+    )
+  end
 end
